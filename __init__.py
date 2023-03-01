e2694d4e1d986c71bd247c980ea3e373ba5cfc83
Shuyu Wu <wushuyu2002@sjtu.edu.cn>
2023-03-01 16:31:50 -0500

    init.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""Insta485 package initializer."""
+import flask
+
+
+# app is a single object used by all the code modules in this package
+app = flask.Flask(__name__)  # pylint: disable=invalid-name
+
+
+
+
+
